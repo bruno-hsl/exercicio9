837d650a8ee990449678b373d51ceb2ccd7d5aa0
Bruno Henrique <130290196+bruno-hsl@users.noreply.github.com>
2023-10-06 09:40:26 -0300

    Update exun3.c

--- exun3.c
+++ exun3.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-   
-int main(){
+
+main(){
     int vetorA[10];
     int vetorB[10];
 
-     for(int i = 0;i < 10; i++){
-        printf("digite os valores: ");
-        scanf("%d", &vetorA);
+    for(int i = 0; i <10; i++){
+        printf("Digite os valores: ");
+        scanf("%d", &vetorA[i]);
     }
 
-     for(int i = 0;i < 10; i++){
-        vetorB[i] = (vetorA[i] * vetorA[i]);
-        printf("VetorA[%d] - vetorB[%d] ", vetorA[i], vetorB[i]);
-        
+     for(int i = 0; i <10; i++){
+       vetorB[i] = vetorA[i] * vetorA[i];
+       printf("\nVetorA[%d] = %d - vetorB[%d] = %d", i , vetorA[i], vetorB[i]);
     }
-    return 0;
+
+    
 }
