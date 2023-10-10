85eb8ac16fce9b8e3960a9c443244ab10885fb8e
Bruno Henrique <130290196+bruno-hsl@users.noreply.github.com>
2023-10-10 19:35:51 -0300

    Update exun13.c

--- exun13.c
+++ exun13.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int valor[5], maior, menor, posicao = 0;
+    int valor[5], maior, menor, posicaomaior, posicaomenor;
 
-    for(int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++) {
         printf("Digite um valor: ");
         scanf("%d", &valor[i]);
     }
 
     maior = valor[0];
-    menor = valor[0]; 
+    menor = valor[0];
+    posicaomaior = 0;
+    posicaomenor = 0;
 
-    for(int i = 1; i < 5; i++) {
-        if(valor[i] > maior) {
-            maior = valor[i]; 
+    for (int i = 1; i < 5; i++) {
+        if (valor[i] > maior) {
+            maior = valor[i];
+            posicaomaior = i;
         }
-        if(valor[i] < menor) {
-            menor = valor[i]; 
+        if (valor[i] < menor) {
+            menor = valor[i];
+            posicaomenor = i;
         }
     }
 
-    printf("Maior valor: %d\n", maior);
-    printf("Menor valor: %d\n", menor);
+    printf("Maior valor: %d (posicao %d)\n", maior, posicaomaior);
+    printf("Menor valor: %d (posicao %d)\n", menor, posicaomenor);
 
     return 0;
 }
