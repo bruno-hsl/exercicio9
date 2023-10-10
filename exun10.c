b8cbc38a1bbba9a8d9706ffc17e7792eede276af
Bruno Henrique <130290196+bruno-hsl@users.noreply.github.com>
2023-10-10 19:15:19 -0300

    Update exun10.c

--- exun10.c
+++ exun10.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int valores[15], media, soma = 0, aluno = 0;
+    int valores[15], soma = 0, aluno = 0;
+    float media;
 
     for (int i = 0; i < 15; i++) {
         printf("Digite a nota do aluno %d: ", aluno + 1);
@@ -10,13 +11,13 @@ int main() {
         aluno++;
     }
 
-    media = soma / 3;
+    media = soma / 15;
 
     for (int i = 0; i < 15; i++) {
         printf("Nota do aluno %d: %d\n", i + 1, valores[i]);
     }
 
-    printf("Media: %d\n", media);
+    printf("Media: %f\n", media);
 
     return 0;
 }
